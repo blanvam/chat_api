459925309ef9cefe5c21124e0ce88fa8a66bbd20
asfarto <blanvam@gmail.com>
2015-12-02 22:46:39 +0100

    Update data to 2.13.21 S40 version

--- lib/chat_api/chat_api.rb
+++ lib/chat_api/chat_api.rb
@@ -19,8 +19,8 @@ module Dora
   WHATSAPP_REQUEST_HOST   = 'v.whatsapp.net/v2/code'     # Request host
   WHATSAPP_SERVER         = 's.whatsapp.net'             # Whatsapp server
   WHATSAPP_DEVICE         = 'S40'                        # The device name.
-  WHATSAPP_VER            = '2.12.96'                    # Version
-  WHATSAPP_USER_AGENT     = 'WhatsApp/2.12.96 S40Version/14.26 Device/Nokia302'        # User agent used in request/registration code.
+  WHATSAPP_VER            = '2.13.21'                    # Version
+  WHATSAPP_USER_AGENT     = 'WhatsApp/2.13.21 S40Version/14.26 Device/Nokia302'        # User agent used in request/registration code.
   WHATSAPP_VER_CHECKER    = 'https://coderus.openrepos.net/whitesoft/whatsapp_scratch' # Check WhatsApp version.
 
 
